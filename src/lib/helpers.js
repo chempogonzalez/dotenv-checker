90888f64b7e235e817333ba7c12f45466060b281
chempogonzalez <manugonzalezfranco@gmail.com>
2022-03-18 20:19:25 +0100

    fix: get proper value when parsing attributes with '=' inside string value

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -6,7 +6,10 @@ const getParsedAttributes = (fileContent) => {
 
     // If it's not a comment line
     if (!line.startsWith('#')) {
-      const [key, value] = line.split('=')
+      const equalsSymbolIdx = line.indexOf('=')
+      const key = line.substring(0, equalsSymbolIdx >= 0 ? equalsSymbolIdx : undefined)
+      const value = equalsSymbolIdx !== -1 ? line.substring(equalsSymbolIdx + 1) : ''
+
       if (key) acc[key] = value ?? ''
     }
 
